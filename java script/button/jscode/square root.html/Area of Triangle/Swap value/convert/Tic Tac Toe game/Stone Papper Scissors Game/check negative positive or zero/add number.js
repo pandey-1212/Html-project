941b6b0a289cec3fe40431208fe490e8730fa137
Pintu Pandey <psp169142021@gmail.com>
2025-04-25 23:41:22 +0530

    Add files via upload

---
+++ java script/button/jscode/square root.html/Area of Triangle/Swap value/convert/Tic Tac Toe game/Stone Papper Scissors Game/check negative positive or zero/add number.js
@@ -0,0 +1,5 @@
+// create a variable of type string and try to add a number to it
+
+let a="Pintu"
+let b="Pandey"
+console.log(a+b)
---
+++ java script/button/script.js
@@ -0,0 +1,11 @@
+let btn1 = document.quarySelector("#btn1");
+
+btn1.onclick = () => {
+    console.log("btn1 was clicked");
+
+};
+
+let div = document.quarySelector("div");
+div.onmouseover = () =>{
+    console.log("you are inside div");
+};
